89e73f542fc31525556aebe5cae874d1a5dd1b6e
Jeremiah Seagraves <j.m.s.p.jerry@gmail.com>
2020-04-24 23:44:33 -0500

    initial commit

---
+++ src/main/java/com/jeremiahseagraves/cloud/azuressok8s/AzureSsoK8sApplication.java
@@ -0,0 +1,13 @@
+package com.jeremiahseagraves.cloud.azuressok8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureSsoK8sApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureSsoK8sApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeremiahseagraves/cloud/azuressok8s/AzureSsoK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeremiahseagraves.cloud.azuressok8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureSsoK8sApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
